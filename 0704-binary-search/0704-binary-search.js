d59e7907e519697429d8eb03c1312b2745c42b93
shivamt24 <shivam.thabe@gmail.com>
2022-10-14 04:26:39 -0400

    Time: 66 ms (95.96%), Space: 45 MB (54.88%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    
+    while(left <= right){
+        let middle = Math.round((left + right)/2);
+        if( nums[middle] === target ){
+            return middle;
+        }
+        if( nums[middle] > target ){
+            right = middle-1;
+        } else {
+            left = middle+1;
+        }
+    }
+    return -1;
+    
+};
