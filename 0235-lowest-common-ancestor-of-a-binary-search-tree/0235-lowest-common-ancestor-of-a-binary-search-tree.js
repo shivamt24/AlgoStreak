dac329fd3fd322460e3adfd3213502fa4889466f
shivamt24 <shivam.thabe@gmail.com>
2022-10-23 01:42:09 -0400

    Time: 88 ms (93.48%), Space: 51.6 MB (94.84%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,19 +14,25 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     
-    let node = root;
+    return lcaHelper(root,p,q);
     
-    while( node !== null ){
-        
-        if( node.val > p.val && node.val > q.val ){
-            node = node.left;
-        }
-        else if( node.val < p.val && node.val < q.val ){
-            node = node.right;
-        } else {
-            return node;
-        }
-        
+}; 
+//Recursive
+
+let lcaHelper = (node,p,q) => {
+    //if(node === null) return;
+    
+    if(node.val > p.val && node.val > q.val){
+        return lcaHelper(node.left,p,q);
     }
     
-};
+    else if(node.val < p.val && node.val < q.val){
+        return lcaHelper(node.right,p,q);
+    }
+    
+    else {
+        return node;
+    } 
+    
+}
+
