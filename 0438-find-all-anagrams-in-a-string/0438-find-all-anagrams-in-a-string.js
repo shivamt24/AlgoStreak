8320e18ef65c51304f846b68858ffbf6c245eb95
shivamt24 <shivam.thabe@gmail.com>
2022-11-03 16:20:21 -0400

    Time: 196 ms (51.52%), Space: 54.1 MB (5.02%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+    
+    let isArrayEqual = (arr1, arr2)=>{
+        for(let i in arr1){
+            if( arr1[i] !== arr2[i] ){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    let sArr = new Array(26).fill(0);
+    let pArr = new Array(26).fill(0);
+    let sol = [];
+    
+    for(let i in p){
+        pArr[  p.charCodeAt(i) - 97 ]++;
+    }
+    
+    for(let i in s){
+        sArr[ s.charCodeAt(i) - 97 ]++;
+        
+        if(i > p.length-1 ){
+            let removeHead = s.charCodeAt( i - p.length );
+            sArr[removeHead - 97]--;
+        }
+        if( i >= p.length - 1){
+            if( isArrayEqual(pArr, sArr) ) sol.push( i - p.length +1 );
+        }
+
+           
+    }
+    return sol;
+    
+};
