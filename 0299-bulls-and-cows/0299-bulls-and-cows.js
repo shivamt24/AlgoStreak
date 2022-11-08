77e5b0d2a014d0e9a0a98a041f8d4935bcd4f085
shivamt24 <shivam.thabe@gmail.com>
2022-11-08 14:51:10 -0500

    Time: 147 ms (17.33%), Space: 44.8 MB (52.45%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.js
+++ 0299-bulls-and-cows/0299-bulls-and-cows.js
@@ -3,6 +3,44 @@
  * @param {string} guess
  * @return {string}
  */
+//single pass
+var getHint = function(secret, guess) {
+    
+    let map = new Map();
+    let a = 0;
+    let b= 0;
+    for(let i in secret){
+        let s = secret[i];
+        let g = guess[i];
+        
+        if(s === g){
+            a++;
+        } else {
+            if( map.get(g) > 0 ){
+                b++;
+            }
+            if( map.get(s) < 0 ){
+                b++;
+            }
+            map.has(g)? map.set(g, map.get(g) - 1 ) : map.set(g,-1);
+            map.has(s)? map.set(s, map.get(s) + 1): map.set(s,1);
+        }
+        
+    }
+    console.log(map);
+        return `${a}`+ "A" + `${b}`+ "B";
+    
+    
+}
+
+
+
+
+
+
+
+/*
+//Double Pass
 var getHint = function(secret, guess) {
   
     let a = 0;
@@ -35,7 +73,6 @@ var getHint = function(secret, guess) {
     
     console.log(a,b,map);
     return `${a}`+ "A" + `${b}`+ "B";
- 
-    
     
 };
+*/
