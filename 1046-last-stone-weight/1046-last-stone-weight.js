49ad7e8e477ae689a9eb4515babffc58ad842321
shivamt24 <shivam.thabe@gmail.com>
2022-11-20 15:20:15 -0500

    Time: 177 ms (5.06%), Space: 45.8 MB (7.78%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+            stones = stones.sort((a,b)=>a-b);
+    while(stones.length !== 1){
+
+
+        
+        let a = stones.pop();
+        let b = stones.pop();
+        let newStone = a-b;
+        if(newStone > 0){
+            stones.push(newStone);
+            stones = stones.sort((a,b)=>a-b);
+                    console.log(stones);
+        } else if(stones.length === 0){
+            return 0;
+        }
+    }
+    console.log(stones);
+    return stones[0];
+};
